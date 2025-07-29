cbe8b50523553d42b1dc2e353deced40c63533ac
오가희 <72989911+ogh010@users.noreply.github.com>
2025-07-29 16:03:45 +0900

    feat : 라우터 RootLaout , 전역 에러 페이지 설정

---
+++ src/app/error/GlovbalErrorPage.tsx
@@ -0,0 +1,22 @@
+import { useRouteError, isRouteErrorResponse } from "react-router-dom";
+
+type Props = {};
+
+export default function GlovbalErrorPage({}: Props) {
+  const error = useRouteError();
+  return (
+    <>
+      {isRouteErrorResponse(error) ? (
+        <div>
+          <h1>{error.status} 에러</h1>
+          <p>{error.statusText}</p>
+        </div>
+      ) : (
+        <div>
+          <h1>알 수 없는 에러 발생</h1>
+          <p>{(error as Error).message}</p>
+        </div>
+      )}
+    </>
+  );
+}
