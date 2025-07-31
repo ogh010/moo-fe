f572d300343c7bed85914f92dbce6bdffa946c6c
오가희 <72989911+ogh010@users.noreply.github.com>
2025-07-31 17:49:40 +0900

    api 프록시 추가 임시

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path"; // ⬅️ 꼭 필요
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080", // todo 백엔드 주소, 프록시할 대상 서버 url을 설정
+        changeOrigin: true, // 원본 호스트 헤더를 변경
+        rewrite: (path) => path.replace(/^\/api/, ""), // 요청 경로를 재작성
+      },
+    },
+  },
+  resolve: {
+    alias: [
+      { find: "@", replacement: path.resolve(__dirname, "src") }, // ✅ 수정됨
+    ],
+  },
 });
