f628fbc6ace2ddd357cca8439d4698c6e793b335
오가희 <ogh010@naver.com>
2025-08-01 23:21:50 +0900

    typescript config

--- vite.config.ts
+++ vite.config.ts
@@ -15,8 +15,14 @@ export default defineConfig({
     },
   },
   resolve: {
-    alias: [
-      { find: "@", replacement: path.resolve(__dirname, "src") }, // ✅ 수정됨
-    ],
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      app: path.resolve(__dirname, "./src/app"),
+      pages: path.resolve(__dirname, "./src/pages"),
+      widgets: path.resolve(__dirname, "./src/widgets"),
+      features: path.resolve(__dirname, "./src/features"),
+      entities: path.resolve(__dirname, "./src/entities"),
+      shared: path.resolve(__dirname, "./src/shared"),
+    },
   },
 });
